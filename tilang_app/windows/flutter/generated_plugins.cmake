8773b2383a7e3bee30ef7a178b4f2a5320df9ff2
zahriaan <abelmuhammadzahrian_2327240042@mhs.mdp.ac.id>
2026-05-01 21:01:23 +0700

    push 4 (menambahkan dependensi)

--- tilang_app/windows/flutter/generated_plugins.cmake
+++ tilang_app/windows/flutter/generated_plugins.cmake
@@ -3,11 +3,18 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
+  file_selector_windows
   firebase_auth
   firebase_core
+  firebase_storage
+  geolocator_windows
+  share_plus
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
