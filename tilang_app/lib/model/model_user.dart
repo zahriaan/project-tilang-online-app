5a44cc0db55560135aa1cba42e1634d2e1b4d840
zahriaan <abelmuhammadzahrian_2327240042@mhs.mdp.ac.id>
2026-05-02 12:32:15 +0700

    push 5 (Model Data)

---
+++ tilang_app/lib/model/model_user.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ModelUser {
+  final String uid; // ID Unik dari Firebase Auth
+  final String nama;
+  final String email;
+  final String? fotoProfil;
+  final int jumlahLaporan; // Untuk statistik di layar profil
+  final bool isDarkMode; // Untuk menyimpan preferensi personalisasi pengguna
+
+  ModelUser({
+    required this.uid,
+    required this.nama,
+    required this.email,
+    this.fotoProfil,
+    this.jumlahLaporan = 0,
+    this.isDarkMode = false,
+  });
+
+  // Mengubah data dari Firestore ke Object Flutter
+  factory ModelUser.fromMap(Map<String, dynamic> data) {
+    return ModelUser(
+      uid: data['uid'] ?? '',
+      nama: data['nama'] ?? '',
+      email: data['email'] ?? '',
+      fotoProfil: data['fotoProfil'],
+      jumlahLaporan: data['jumlahLaporan'] ?? 0,
+      isDarkMode: data['isDarkMode'] ?? false,
+    );
+  }
+
+  // Mengubah Object Flutter ke Map untuk disimpan di Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'nama': nama,
+      'email': email,
+      'fotoProfil': fotoProfil,
+      'jumlahLaporan': jumlahLaporan,
+      'isDarkMode': isDarkMode,
+    };
+  }
+}
